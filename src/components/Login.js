6b43b628c32d32cece9783f1102626c47a5cf718
SpaceArk& <spacearkchief@gmail.com>
2021-01-29 17:22:49 +0700

    up

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,57 +1,59 @@
-import {AiFillEye} from 'react-icons/all'
-import {Link} from 'react-router-dom'
-import '../Style/Login.css'
-
+import { AiFillEye } from "react-icons/all";
+import { Link, useHistory } from "react-router-dom";
+import "../Style/Login.css";
 
 const Login = () => {
-    return (    
+  const history = useHistory();
+
+  function handleSubmit() {
+    return history.push("/");
+  }
+
+  return (
     <div className="Login-Container">
-    <div className="Login-Header"></div>
-    <div className="Login-card">
-    
+      <div className="Login-Header"></div>
+      <div className="Login-card">
         <div className="Login-form">
-            <div className="Login-form-text">
+          <div className="Login-form-text">
             <div className="Login-title">
-                <h2>Masuk Ke Akun LabQ</h2>
+              <h2>Masuk Ke Akun LabQ</h2>
             </div>
 
-            
-        
             <div className="Login-toLogin">
-                <p>Belum punya akun? <Link to='/signIn'>Daftar</Link></p>
+              <p>
+                Belum punya akun? <Link to="/signIn">Daftar</Link>
+              </p>
             </div>
+          </div>
+
+          <form onSubmit={handleSubmit}>
+            <div className="inputBox">
+              <p>Nama Pengguna atau Email</p>
+              <input required type="text" name="username" id="userId" />
             </div>
-            
 
-            <form>
-                <div className="inputBox"> 
-                    <p>Nama Pengguna atau Email</p>
-                    <input required type="text" name="username" id="userId"/>
-                </div>
-               
+            <div className="inputBox ">
+              <p>Password</p>
+              <div className="pwbox">
+                <input required type="password" name="password" id="pwdId" />{" "}
+                <AiFillEye className="eyeicon" />
+              </div>
+            </div>
 
-              
-                           
-                <div className="inputBox ">
-                    <p>Password</p>
-                    <div className="pwbox"><input required type="password" name="password" id="pwdId" /> <AiFillEye className="eyeicon" /></div>
-                    
-                </div>
+            <div className="inputBox ">
+              <p>Konfirmasi Password</p>
+              <div className="confirm-pwbox">
+                <input required type="password" name="password" id="ConpwdId" />{" "}
+                <AiFillEye className="eyeicon" />
+              </div>
+            </div>
 
-                <div className="inputBox ">
-                    <p>Konfirmasi Password</p>
-                    <div className="confirm-pwbox"><input required type="password" name="confirm-password" id="ConpwdId" /> <AiFillEye className="eyeicon" /></div>
-                    
-                </div>
-                
-                <button type="submit">Masuk</button>
-            </form>
-
-            
-
+            <button type="submit">Masuk</button>
+          </form>
         </div>
+      </div>
     </div>
-</div> );
-}
- 
+  );
+};
+
 export default Login;
